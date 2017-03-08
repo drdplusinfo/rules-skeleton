ba5b0e624769490604bfc39e136115fe3c42278c
Jaroslav Týc <mail@jaroslavtyc.com>
2017-03-08 11:44:09 +0100

    Tests bootstrap example added

---
+++ tests/tests_bootstrap_example.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types = 1); // on PHP 7+ are standard PHP methods strict to types of given parameters
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(-1);
+ini_set('display_errors', '1');
+
+ini_set('xdebug.max_nesting_level', '100');
+
+const DRD_PLUS_RULES_INDEX_FILE_NAME_TO_TEST = __DIR__ . '/../index.php';
+const DRD_PLUS_RULES_DIR_TO_TEST = __DIR__ . '/..';
