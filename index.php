5ef9d38643d66eb9942d6064c679e171948a3a09
Jaroslav Týc <mail@jaroslavtyc.com>
2017-03-09 10:12:31 +0100

    Errors are shown when ran via CLI as well

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(-1);
-if (!empty($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] === '127.0.0.1') {
+if ((!empty($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] === '127.0.0.1') || PHP_SAPI === 'cli') {
     ini_set('display_errors', '1');
 } else {
     ini_set('display_errors', '0');
