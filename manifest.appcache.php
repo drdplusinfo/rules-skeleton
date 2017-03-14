a3242aab9e2be500393c7704f2c5340096499815
Jaroslav Týc <mail@jaroslavtyc.com>
2017-03-14 13:49:12 +0100

    Simplified no-cache headers for manifest

--- manifest.appcache.php
+++ manifest.appcache.php
@@ -4,11 +4,9 @@ $documentRoot = rtrim(!empty($_SERVER['SCRIPT_FILENAME']) ? dirname($_SERVER['SC
 require_once $documentRoot . '/vendor/autoload.php';
 
 header('Content-Type: text/cache-manifest');
-header('Expires: on, 01 Jan 1970 00:00:00 GMT');
-header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
-header('Cache-Control: no-store, no-cache, must-revalidate');
-header('Cache-Control: post-check=0, pre-check=0', false);
-header('Pragma: no-cache');
+header('Cache-Control: no-cache, no-store, must-revalidate'); // HTTP 1.1
+header('Pragma: no-cache'); // HTTP 1.0
+header('Expires: 0'); // Proxies
 
 $manifestAccessCount = ($_COOKIE['manifestAccessCount'] ?? 0) + 1;
 setcookie('manifestAccessCount', $manifestAccessCount);
