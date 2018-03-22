abb94be866d5d3f852b27439bf30c4c29f580ad8
Jaroslav Týc <mail@jaroslavtyc.com>
2018-03-22 15:43:49 +0100

    Mutex for upcomming version switcher

---
+++ DrdPlus/RulesSkeleton/VersionMutex.php
@@ -0,0 +1,94 @@
+<?php
+declare(strict_types=1);
+/** be strict for parameter types, https://www.quora.com/Are-strict_types-in-PHP-7-not-a-bad-idea */
+
+namespace DrdPlus\RulesSkeleton;
+
+use Granam\Strict\Object\StrictObject;
+
+class VersionMutex extends StrictObject
+{
+
+    /**
+     * @var string
+     */
+    private $lockDir;
+    /**
+     * @var null|resource
+     */
+    private $lockFileHandle;
+
+    public function __construct(string $lockDir = null)
+    {
+        $this->lockDir = $lockDir ?? \sys_get_temp_dir();
+    }
+
+    /**
+     * @param int $wait
+     * @return bool
+     * @throws \DrdPlus\RulesSkeleton\Exceptions\CanNotWriteLockOfVersionMutex
+     * @throws \DrdPlus\RulesSkeleton\Exceptions\CanNotLockVersionMutex
+     */
+    public function lock(int $wait = 10): bool
+    {
+        $waitUntil = \time() + $wait;
+        $locked = null;
+        $handle = $this->getLockFileHandle();
+        $attempts = 0;
+        do {
+            $attempts++;
+            if ($locked !== null) {
+                \sleep(1);
+            }
+            $locked = \flock($handle, LOCK_EX | LOCK_NB);
+        } while (!$locked && \time() < $waitUntil);
+        if (!$locked) {
+            throw new Exceptions\CanNotLockVersionMutex(
+                "Even after {$wait} seconds and {$attempts} attempts the lock has not been obtained"
+            );
+        }
+        \fwrite($handle, (string)\time());
+
+        return true;
+    }
+
+    /**
+     * @return resource
+     * @throws \DrdPlus\RulesSkeleton\Exceptions\CanNotWriteLockOfVersionMutex
+     */
+    private function getLockFileHandle()
+    {
+        if (!$this->lockFileHandle) {
+            $this->lockFileHandle = \fopen($this->getLockFileName(), 'wb');
+            if (!$this->lockFileHandle) {
+                throw new Exceptions\CanNotWriteLockOfVersionMutex(
+                    "Can not use {$this->getLockFileName()} as a lock file, can not write to it"
+                );
+            }
+        }
+
+        return $this->lockFileHandle;
+    }
+
+    private function getLockFileName(): string
+    {
+        return $this->lockDir . '/drdplus_rules_version_mutex';
+    }
+
+    public function __destruct()
+    {
+        $this->unlock();
+    }
+
+    public function unlock(): bool
+    {
+        if (!$this->lockFileHandle) {
+            return false;
+        }
+        $unlocked = \flock($this->lockFileHandle, LOCK_UN | LOCK_NB); // it is no harm to unlock it even if it was not locked
+        \fclose($this->lockFileHandle);
+        $this->lockFileHandle = null;
+
+        return $unlocked;
+    }
+}
---
+++ Tests/DrdPlus/RulesSkeleton/VersionMutexTest.php
@@ -0,0 +1,43 @@
+<?php
+namespace Tests\DrdPlus\RulesSkeleton;
+
+use DrdPlus\RulesSkeleton\VersionMutex;
+use PHPUnit\Framework\TestCase;
+
+class VersionMutexTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function I_can_get_and_release_lock(): void
+    {
+        $mutex = new VersionMutex();
+        self::assertTrue($mutex->lock(), 'Can not get lock via mutex');
+        self::assertTrue($mutex->unlock(), 'Can not unlock mutex');
+        self::assertFalse($mutex->unlock(), 'Second unlock in a row should NOT be successful');
+    }
+
+    /**
+     * @test
+     */
+    public function I_can_not_lock_twice(): void
+    {
+        $mutex = new VersionMutex();
+        $mutex->lock();
+        $message = \exec(<<<'PHP'
+php -r 'include "vendor/autoload.php";
+try {
+    (new DrdPlus\RulesSkeleton\VersionMutex)->lock(0 /* no wait */);
+} catch(\DrdPlus\RulesSkeleton\Exceptions\CanNotLockVersionMutex $exception) {
+    echo $exception->getMessage();
+    exit(0);
+}
+exit(1);'
+PHP
+        , $output,
+        $returnCode
+);
+
+        self::assertSame(0, $returnCode, $message);
+    }
+}
