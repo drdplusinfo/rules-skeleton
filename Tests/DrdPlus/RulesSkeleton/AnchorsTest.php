1b85865a7dd9f0b9121d5706c95eff0e8f4fa502
Jaroslav Týc <mail@jaroslavtyc.com>
2018-03-27 15:45:24 +0200

    Anchors tests are ran in separate process to avoid problem with switched version but previous code usage

--- Tests/DrdPlus/RulesSkeleton/AnchorsTest.php
+++ Tests/DrdPlus/RulesSkeleton/AnchorsTest.php
@@ -16,6 +16,7 @@ class AnchorsTest extends AbstractContentTest
 
     /**
      * @test
+     * @runInSeparateProcess
      */
     public function All_anchors_point_to_syntactically_valid_links(): void
     {
