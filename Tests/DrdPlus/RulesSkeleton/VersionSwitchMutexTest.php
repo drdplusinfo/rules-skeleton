4d1b8f2f234dd2885fe62ffe4f30cb3f3318c7c2
Jaroslav Týc <mail@jaroslavtyc.com>
2018-03-28 08:56:33 +0200

    Method to test lock ID tested

--- Tests/DrdPlus/RulesSkeleton/VersionSwitchMutexTest.php
+++ Tests/DrdPlus/RulesSkeleton/VersionSwitchMutexTest.php
@@ -12,8 +12,14 @@ class VersionSwitchMutexTest extends TestCase
     public function I_can_get_and_release_lock(): void
     {
         $mutex = new VersionSwitchMutex();
-        self::assertTrue($mutex->lock(), 'Can not get lock via mutex');
+        self::assertFalse($mutex->isLockedForId('foo'), 'Should not be locked yet');
+        self::assertFalse($mutex->isLockedForId('bar'), 'Should not be locked yet');
+        self::assertTrue($mutex->lock('foo'), 'Can not get lock via mutex');
+        self::assertTrue($mutex->isLockedForId('foo'), 'Should be locked for "foo" version');
+        self::assertFalse($mutex->isLockedForId('bar'), 'Should be locked for different version');
         self::assertTrue($mutex->unlock(), 'Can not unlock mutex');
+        self::assertFalse($mutex->isLockedForId('foo'), 'Should be already unlocked');
+        self::assertFalse($mutex->isLockedForId('bar'), 'Should be already unlocked');
         self::assertFalse($mutex->unlock(), 'Second unlock in a row should NOT be successful');
     }
 
@@ -23,11 +29,11 @@ class VersionSwitchMutexTest extends TestCase
     public function I_can_not_lock_twice(): void
     {
         $mutex = new VersionSwitchMutex();
-        $mutex->lock();
+        $mutex->lock('foo');
         $message = \exec(<<<'PHP'
 php -r 'include "vendor/autoload.php";
 try {
-    (new DrdPlus\RulesSkeleton\VersionSwitchMutex)->lock(0 /* no wait */);
+    (new DrdPlus\RulesSkeleton\VersionSwitchMutex)->lock("bar", 0 /* no wait */);
 } catch(\DrdPlus\RulesSkeleton\Exceptions\CanNotLockVersionMutex $exception) {
     echo $exception->getMessage();
     exit(0);
