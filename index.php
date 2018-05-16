f21ab6f189459153b989a76f6795a94d2267c602
Jaroslav Týc <mail@jaroslavtyc.com>
2018-05-16 17:29:09 +0200

    fix: Assets script and index can be ran from vendor dir

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
-$documentRoot = __DIR__;
+$documentRoot = $documentRoot ?? __DIR__;
+
+$dirToScanForFrontendSkeleton = $documentRoot;
+$currentDir = \str_replace('\\', '/', __DIR__);
+if (\strpos($currentDir, '/vendor/') !== false) {
+    $dirToScanForFrontendSkeleton = \substr($currentDir, 0, \strpos($currentDir, '/vendor/'));
+}
+do {
+    foreach (\scandir($dirToScanForFrontendSkeleton, SCANDIR_SORT_NONE) as $folder) {
+        if ($folder === 'vendor') {
+            $frontendSkeletonIndex = $dirToScanForFrontendSkeleton . '/vendor/drd-plus/frontend-skeleton/index.php';
+            if (\file_exists($frontendSkeletonIndex)) {
+                /** @noinspection PhpIncludeInspection */
+                require $frontendSkeletonIndex;
+
+                return;
+            }
+            unset($frontendSkeletonIndex);
+        }
+    }
+    $dirToScanForFrontendSkeleton .= '/..';
+} while (\is_readable($dirToScanForFrontendSkeleton));
+unset($dirToScanForFrontendSkeleton, $currentDir);
+
 require __DIR__ . '/vendor/drd-plus/frontend-skeleton/index.php';
