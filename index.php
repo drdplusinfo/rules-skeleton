24f6f77d85b29b41f598fd0cf7e13bc4f55eda8a
Jaroslav Týc <mail@jaroslavtyc.com>
2018-05-18 18:17:05 +0200

    fix: vendor root is defined relatively to document root

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $documentRoot = $documentRoot ?? __DIR__;
 
-$vendorRoot = $vendorRoot ?? __DIR__ . '/vendor';
+$vendorRoot = $vendorRoot ?? $documentRoot . '/vendor';
 $partsRoot = __DIR__ . '/parts';
 if (\file_exists($vendorRoot . '/drd-plus/frontend-skeleton/index.php')) {
     require $vendorRoot . '/drd-plus/frontend-skeleton/index.php';
