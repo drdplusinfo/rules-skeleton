f3971e04c9b46844efb2678acba5ff6345912065
Jaroslav Týc <mail@jaroslavtyc.com>
2018-05-20 15:03:33 +0200

    Libraries updated

--- js/generic/frontend-skeleton/versions.js
+++ js/generic/frontend-skeleton/versions.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     var currentVersion = document.getElementsByClassName('current-version')[0];
+    if (typeof currentVersion === 'undefined') {
+        return;
+    }
     var otherVersions = document.getElementsByClassName('other-versions')[0];
+    if (typeof otherVersions === 'undefined') {
+        return;
+    }
     currentVersion.addEventListener('click', function () {
-        console.log(otherVersions.style.display);
         if (otherVersions.style.display === 'none' || !otherVersions.style.display) {
             otherVersions.style.display = 'block';
         } else {
