2114e2577bd3eb6425d78b29741b93d9777b004d
Jaroslav Týc <mail@jaroslavtyc.com>
2018-05-22 16:27:25 +0200

    Fixed tables-only mode test to be configurable

--- DrdPlus/Tests/RulesSkeleton/HtmlHelperTest.php
+++ DrdPlus/Tests/RulesSkeleton/HtmlHelperTest.php
@@ -4,32 +4,11 @@ declare(strict_types=1);
 
 namespace DrdPlus\Tests\RulesSkeleton;
 
-use DrdPlus\FrontendSkeleton\HtmlHelper;
-use Gt\Dom\Element;
-
 class HtmlHelperTest extends \DrdPlus\Tests\FrontendSkeleton\HtmlHelperTest
 {
-    /**
-     * @test
-     */
-    public function I_can_get_filtered_tables_from_content(): void
+    protected function getSomeExpectedTableIds(): array
     {
-        $htmlHelper = HtmlHelper::createFromGlobals($this->getDocumentRoot());
-
-        $allTables = $htmlHelper->findTablesWithIds($this->getHtmlDocument());
-        self::assertCount(2, $allTables);
-        foreach (['iamsoalone', 'justsometable'] as $expectedTableId) {
-            self::assertArrayHasKey($expectedTableId, $allTables);
-            $table = $allTables[$expectedTableId];
-            self::assertInstanceOf(Element::class, $table);
-            self::assertNotEmpty($table->innerHTML);
-            self::assertSame($expectedTableId, $table->getAttribute('id'));
-        }
-
-        self::assertEmpty($htmlHelper->findTablesWithIds($this->getHtmlDocument(), ['nonExistingTableId']));
-
-        $singleTable = $htmlHelper->findTablesWithIds($this->getHtmlDocument(), ['JustSomeTable']);
-        self::assertCount(1, $singleTable);
-        self::assertArrayHasKey('justsometable', $singleTable, 'ID is expected to be lower-cased');
+        return ['IAmSoAlone', 'JustSomeTable'];
     }
+
 }
