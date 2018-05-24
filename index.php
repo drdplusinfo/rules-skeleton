6d30a10fc353b6e016b872bfc3b1bbca41d3f095
Jaroslav Týc <mail@jaroslavtyc.com>
2018-05-24 13:54:22 +0200

    fix: Parts dir is loaded from vendor if there is no one in document root

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 $documentRoot = $documentRoot ?? (PHP_SAPI !== 'cli' ? \rtrim(\dirname($_SERVER['SCRIPT_FILENAME']), '\/') : \getcwd());
 $vendorRoot = $vendorRoot ?? $documentRoot . '/vendor';
-$partsRoot = $documentRoot . '/parts';
+$partsRoot = \file_exists($documentRoot . '/parts')
+    ? ($documentRoot . '/parts')
+    : ($vendorRoot . '/drd-plus/rules-skeleton/parts');
 
 require $vendorRoot . '/drd-plus/frontend-skeleton/index.php';
