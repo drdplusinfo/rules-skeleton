52cf0982e9c43d0c0d57dc170f4fedfbdb8fa9aa
Jaroslav Týc <mail@jaroslavtyc.com>
2018-06-08 09:29:50 +0200

    Pass beautify

--- web/pass/02 licence manifest.php
+++ web/pass/02 licence manifest.php
@@ -8,32 +8,49 @@ $eShopUrl = $controller->getEshopUrl();
 ?>
 <div class="row">
   <form class="manifest trial" action="" method="post">
-    <p>
-      <label>
-        <input type="submit" name="trial" value="Juknu na <?= $webName ?>">
-        <span class="col-6">Chci se na <strong><?= $webName ?></strong> jen na chvíli podívat, ať vím, o co jde</span>
-      </label>
-    </p>
+    <div class="col">
+      <button type="submit" id="trial" name="trial" value="trial">Juknu na <?= $webName ?></button>
+    </div>
+    <div class="col">
+      <ul>
+        <li>
+          <label for="trial">chci se na <strong><?= $webName ?></strong> jen na chvíli podívat, ať vím, o co
+            jde</label>
+        </li>
+      </ul>
+    </div>
   </form>
 </div>
 <div class="row">
   <form class="manifest buy" action="<?= $eShopUrl ?>">
-    <p>
-      <label>
-        <input class="col" type="submit" name="buy" value="Koupím <?= $webName ?>">
-        <span class="col">Zatím nemám <strong><?= $webName ?></strong>, tak si je od Altaru koupím <span class="note">(doporučujeme PDF verzi)</span></span>
-      </label>
-    </p>
+    <div class="col">
+      <button type="submit" id="buy" name="buy" value="buy">Koupím <?= $webName ?></button>
+    </div>
+    <div class="col">
+      <ul>
+        <li>
+          <label for="buy">
+            zatím nemám <strong><?= $webName ?></strong>, tak si je od Altaru koupím <span class="note">(doporučujeme PDF verzi)</span>
+          </label>
+        </li>
+      </ul>
+    </div>
   </form>
 </div>
 <div class="row">
   <form class="manifest owning" action="" method="post"
         onsubmit="return window.confirm('A klidně to potvrdím dvakrát')">
-    <p>
-      <label>
-        <input class="col" type="submit" name="confirm" value="Vlastním <?= $webName ?>">
-        <span class="col">Prohlašuji na svou čest, že vlastním legální kopii <strong><?= $webName ?></strong></span>
-      </label>
-    </p>
+    <div class="col">
+      <button type="submit" id="confirm" name="confirm" value="submit">Vlastním <?= $webName ?></button>
+    </div>
+    <div class="col">
+      <ul>
+        <li>
+          <label for="confirm">
+            prohlašuji na svou čest, že vlastním legální kopii <strong><?= $webName ?></strong>
+          </label>
+        </li>
+      </ul>
+    </div>
   </form>
 </div>
