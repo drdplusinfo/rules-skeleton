759115d7e41e1a48e85f576b10cd86d0187e1e55
Jaroslav Týc <mail@jaroslavtyc.com>
2018-06-22 14:48:35 +0200

    Updated underlying skeleton with Google analytics

--- DrdPlus/Tests/RulesSkeleton/ComposerConfigTest.php
+++ DrdPlus/Tests/RulesSkeleton/ComposerConfigTest.php
@@ -1,43 +1,12 @@
 <?php
 namespace DrdPlus\Tests\RulesSkeleton;
 
-use DrdPlus\Tests\FrontendSkeleton\AbstractContentTest;
-
 /**
  * @method TestsConfiguration getTestsConfiguration
  */
-class ComposerConfigTest extends AbstractContentTest
+class ComposerConfigTest extends \DrdPlus\Tests\FrontendSkeleton\ComposerConfigTest
 {
     use AbstractContentTestTrait;
-
-    protected static $composerConfig;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-        if (static::$composerConfig === null) {
-            $composerFilePath = $this->getDocumentRoot() . '/composer.json';
-            self::assertFileExists($composerFilePath, 'composer.json has not been found in document root');
-            $content = \file_get_contents($composerFilePath);
-            self::assertNotEmpty($content, "Nothing has been fetched from $composerFilePath, is readable?");
-            static::$composerConfig = \json_decode($content, true /*as array */);
-            self::assertNotEmpty(static::$composerConfig, 'Can not decode composer.json content');
-        }
-    }
-
-    /**
-     * @test
-     */
-    public function Project_is_using_php_with_nullable_type_hints(): void
-    {
-        $requiredPhpVersion = static::$composerConfig['require']['php'];
-        self::assertGreaterThan(0, \preg_match('~(?<version>\d.+)$~', $requiredPhpVersion, $matches));
-        $minimalPhpVersion = $matches['version'];
-        self::assertGreaterThanOrEqual(
-            0,
-            \version_compare($minimalPhpVersion, '7.1'), "Required PHP version should be equal or greater to 7.1, get $requiredPhpVersion"
-        );
-    }
 
     /**
      * @test
@@ -50,25 +19,6 @@ class ComposerConfigTest extends AbstractContentTest
             static::$composerConfig['license'],
             "Expected licence '$expectedLicence' as content is " . ($this->getTestsConfiguration()->hasProtectedAccess() ? 'not public' : 'public')
         );
-    }
-
-    /**
-     * @test
-     */
-    public function Assets_have_checked_versions(): void
-    {
-        $postInstallScripts = static::$composerConfig['scripts']['post-install-cmd'] ?? [];
-        self::assertNotEmpty($postInstallScripts, 'Missing post-install-cmd scripts');
-        $postUpdateScripts = static::$composerConfig['scripts']['post-update-cmd'] ?? [];
-        self::assertNotEmpty($postUpdateScripts, 'Missing post-update-cmd scripts');
-        foreach ([$postInstallScripts, $postUpdateScripts] as $postChangeScripts) {
-            self::assertContains(
-                'php ./vendor/bin/assets --css --dir=css',
-                $postChangeScripts,
-                'Missing script to compile assets, there are configs '
-                . \preg_replace('~^Array\n\((.+)\)~', '$1', \var_export($postChangeScripts, true))
-            );
-        }
     }
 
     /**
