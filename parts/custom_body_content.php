b515643e42ff454d7c1b4b25fb8491c8fbd8c490
Jaroslav Týc <mail@jaroslavtyc.com>
2018-08-22 18:02:10 +0200

    Removed support of custom body content

--- parts/custom_body_content.php
+++
@@ -1,6 +0,0 @@
-<span class="invisible">Just some rules HTML skeleton custom body content</span>
-
-<?php
-/** @var \DrdPlus\RulesSkeleton\RulesController $controller */
-/** @noinspection PhpIncludeInspection */
-echo include $controller->getConfiguration()->getDirs()->getGenericPartsRoot() . '/debug_contacts.html';
