8f0c97de5d36bd550ffd10875d79bc252408758e
Jaroslav Týc <mail@jaroslavtyc.com>
2018-08-27 06:36:43 +0200

    Presence of two boolean configurations is tested

--- DrdPlus/RulesSkeleton/Configuration.php
+++ DrdPlus/RulesSkeleton/Configuration.php
@@ -9,13 +9,15 @@ namespace DrdPlus\RulesSkeleton;
  */
 class Configuration extends \DrdPlus\FrontendSkeleton\Configuration
 {
-    public const FREE_ACCESS = 'free_access';
+    public const PROTECTED_ACCESS = 'protected_access';
     public const HIDE_HOME_BUTTON = 'hide_home_button';
     public const ESHOP_URL = 'eshop_url';
 
     public function __construct(Dirs $dirs, array $settings)
     {
         $this->guardValidEshopUrl($settings);
+        $this->guardSetIfHasProtectedAccess($settings);
+        $this->guardSetIfHasShownHomeButton($settings);
         parent::__construct($dirs, $settings);
     }
 
@@ -25,17 +27,37 @@ class Configuration extends \DrdPlus\FrontendSkeleton\Configuration
      */
     protected function guardValidEshopUrl(array $settings): void
     {
-        if (!\filter_var($settings[self::WEB][self::ESHOP_URL] ?? '', FILTER_VALIDATE_URL)) {
+        if (!\filter_var($settings[static::WEB][static::ESHOP_URL] ?? '', FILTER_VALIDATE_URL)) {
             throw new Exceptions\InvalidEshopUrl(
                 'Given e-shop URL is not valid, expected some URL in configuration '
-                . static::WEB . ': ' . static::ESHOP_URL . ', got ' . ($settings[self::WEB][self::ESHOP_URL] ?? 'nothing')
+                . static::WEB . ': ' . static::ESHOP_URL . ', got ' . ($settings[static::WEB][static::ESHOP_URL] ?? 'nothing')
             );
         }
     }
 
-    public function hasFreeAccess(): bool
+    protected function guardSetIfHasProtectedAccess(array $settings): void
     {
-        return (bool)$this->getSettings()[self::WEB][self::FREE_ACCESS];
+        if (($settings[static::WEB][static::PROTECTED_ACCESS] ?? null) === null) {
+            throw new Exceptions\MissingProtectedAccessConfiguration(
+                'Configuration if web has protected access is missing in configuration '
+                . static::WEB . ': ' . static::PROTECTED_ACCESS
+            );
+        }
+    }
+
+    protected function guardSetIfHasShownHomeButton(array $settings): void
+    {
+        if (($settings[static::WEB][static::SHOW_HOME_BUTTON] ?? null) === null) {
+            throw new Exceptions\MissingShownHomeButtonConfiguration(
+                'Configuration if home button should be shown is missing in configuration '
+                . static::WEB . ': ' . static::SHOW_HOME_BUTTON
+            );
+        }
+    }
+
+    public function hasProtectedAccess(): bool
+    {
+        return (bool)$this->getSettings()[self::WEB][self::PROTECTED_ACCESS];
     }
 
     public function shouldHideHomeButton(): bool
