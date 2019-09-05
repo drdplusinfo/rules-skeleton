df86281f24d72767b0fb63ddc4c38edba124275b
Jaroslav Týc <mail@jaroslavtyc.com>
2019-09-05 07:49:20 +0200

    Solved contacts overflow on small screens

--- DrdPlus/RulesSkeleton/Web/content/menu.php
+++ DrdPlus/RulesSkeleton/Web/content/menu.php
@@ -24,44 +24,38 @@ HTML;
       <?= $homeButton ?>
     <span class="contact">
       <a href="mailto:info@drdplus.info">
-        <span class="mobile"><i class="fas fa-envelope"></i></span>
-        <span class="tablet">info@drdplus.info</span>
-        <span class="desktop"><i class="fas fa-envelope"></i> info@drdplus.info</span>
+        <span class="small-screen"><i class="fas fa-envelope"></i></span>
+        <span class="wide-screen"><i class="fas fa-envelope"></i> info@drdplus.info</span>
       </a>
     </span>
     <span class="contact">
       <a target="_blank" class="rpgforum-contact" href="https://rpgforum.cz/forum/viewtopic.php?f=238&t=14870">
-        <span class="mobile"><i class="fas fa-dice-six"></i></span>
-        <span class="tablet">RPG fórum</span>
-        <span class="desktop"><i class="fas fa-dice-six"></i> RPG fórum</span>
+        <span class="small-screen"><i class="fas fa-dice-six"></i></span>
+        <span class="wide-screen"><i class="fas fa-dice-six"></i> RPG fórum</span>
       </a>
     </span>
     <span class="contact">
       <a target="_blank" class="discord-contact" href="https://discordapp.com/invite/FVz5V3Q">
-        <span class="mobile"><i class="fab fa-discord"></i></span>
-        <span class="tablet">Discord</span>
-        <span class="desktop"><i class="fab fa-discord"></i> Discord</span>
+        <span class="small-screen"><i class="fab fa-discord"></i></span>
+        <span class="wide-screen"><i class="fab fa-discord"></i> Discord</span>
       </a>
     </span>
     <span class="contact">
       <a target="_blank" class="twitter-contact" href="https://twitter.com/DrdInfo">
-        <span class="mobile"><i class="fab fa-twitter-square"></i></span>
-        <span class="tablet">Twitter</span>
-        <span class="desktop"><i class="fab fa-twitter-square"></i> Twitter</span>
+        <span class="small-screen"><i class="fab fa-twitter-square"></i></span>
+        <span class="wide-screen"><i class="fab fa-twitter-square"></i> Twitter</span>
       </a>
     </span>
     <span class="contact">
       <a target="_blank" class="facebook-contact" href="https://www.facebook.com/drdplus.info">
-        <span class="mobile"><i class="fab fa-facebook-square"></i></span>
-        <span class="tablet">Facebook</span>
-        <span class="desktop"><i class="fab fa-facebook-square"></i> Facebook</span>
+        <span class="small-screen"><i class="fab fa-facebook-square"></i></span>
+        <span class="wide-screen"><i class="fab fa-facebook-square"></i> Facebook</span>
       </a>
     </span>
     <span class="contact">
       <a target="_blank" class="trello-contact" href="https://trello.com/b/L64FNYj3/drdplusinfo">
-        <span class="mobile"><i class="fab fa-trello"></i></span>
-        <span class="tablet">Trello</span>
-        <span class="desktop"><i class="fab fa-trello"></i> Trello</span>
+        <span class="small-screen"><i class="fab fa-trello"></i></span>
+        <span class="wide-screen"><i class="fab fa-trello"></i> Trello</span>
       </a>
     </span>
   </div>
