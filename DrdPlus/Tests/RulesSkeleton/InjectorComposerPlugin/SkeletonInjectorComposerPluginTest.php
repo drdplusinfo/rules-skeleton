2d56ae9a20479ae8684be47dc1177542c250237b
Jaroslav Týc <mail@jaroslavtyc.com>
2019-12-12 16:46:26 +0100

    Skeleton injection is gully tested

--- DrdPlus/Tests/RulesSkeleton/InjectorComposerPlugin/SkeletonInjectorComposerPluginTest.php
+++ DrdPlus/Tests/RulesSkeleton/InjectorComposerPlugin/SkeletonInjectorComposerPluginTest.php
@@ -43,4 +43,145 @@ class SkeletonInjectorComposerPluginTest extends AbstractContentTest
         self::assertSame(SkeletonInjectorComposerPlugin::class, $this->getComposerConfig()['extra']['class']);
         self::assertArrayHasKey('composer-plugin-api', $this->getComposerConfig()['require']);
     }
+
+    /**
+     * @test
+     */
+    public function Injection_works(): void
+    {
+        if (!$this->isRulesSkeletonChecked()) {
+            self::assertFalse(false, 'Intended for skeleton only');
+            return;
+        }
+        $testDir = sys_get_temp_dir() . '/' . uniqid(basename(__FILE__, '.php'), true);
+        self::assertTrue(
+            @mkdir($testDir, 0755, true) || is_dir($testDir),
+            "Can not create dir $testDir"
+        );
+
+        exec(sprintf('cd %s 2>&1', escapeshellarg($testDir)), $output, $returnCode);
+        self::assertSame(
+            0,
+            $returnCode,
+            'Something goes wrong ' . implode(',', $output)
+        );
+
+        $skeletonRootDir = $this->getProjectRoot();
+        $escapedSkeletonRootDir = json_encode($skeletonRootDir, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        file_put_contents(
+            $testDir . '/composer.json',
+            <<<JSON
+{
+  "require": {
+    "drdplus/rules-skeleton": "dev-master"
+  },
+  "repositories": [
+    {
+        "url": $escapedSkeletonRootDir,
+        "type": "git"
+    }
+  ]
+}
+JSON
+        );
+
+        $testDirEscaped = escapeshellarg($testDir);
+
+        // favicon should be overwritten
+        exec("touch $testDirEscaped/favicon.ico 2>&1", $output, $returnCode);
+        self::assertSame(
+            0,
+            $returnCode,
+            'Something goes wrong ' . implode("\n", $output)
+        );
+
+        exec("cd $testDirEscaped && composer require drdplus/rules-skeleton:dev-master 2>&1 && ls -al images", $output, $returnCode);
+        self::assertSame(
+            0,
+            $returnCode,
+            'Something goes wrong ' . implode("\n", $output)
+        );
+
+        $this->projectConfigIsCopied($testDir);
+        $this->cacheDirIsCreatedWithGitIgnore($testDir);
+        $this->faviconIsCopied($testDir);
+        $this->routesArePopulated($testDir);
+        $this->indexIsPopulated($testDir);
+        $this->webDirIsCreated($testDir);
+    }
+
+    private function projectConfigIsCopied(string $dir)
+    {
+        $projectConfigFile = $this->getProjectRoot() . '/config.distribution.yml';
+        self::assertFileExists($projectConfigFile);
+
+        $copiedProjectConfigFile = $dir . '/config.distribution.yml';
+
+        self::assertFileExists($copiedProjectConfigFile);
+        self::assertFileEquals($projectConfigFile, $copiedProjectConfigFile);
+    }
+
+    private function cacheDirIsCreatedWithGitIgnore(string $dir)
+    {
+        $cacheDir = $dir . '/cache';
+        self::assertDirectoryExists($cacheDir);
+        self::assertDirectoryIsWritable($cacheDir);
+
+        $cacheGitIgnoreFile = $cacheDir . '/.gitignore';
+        self::assertFileExists($cacheGitIgnoreFile);
+        self::assertSame(<<<TEXT
+*
+!/.gitignore
+TEXT
+            , file_get_contents($cacheGitIgnoreFile)
+        );
+    }
+
+    private function faviconIsCopied(string $dir)
+    {
+        $faviconFile = $this->getProjectRoot() . '/favicon.ico';
+        self::assertFileExists($faviconFile);
+        self::assertNotSame(0, filesize($faviconFile));
+
+        $copiedFaviconFile = $dir . '/favicon.ico';
+
+        self::assertFileExists($copiedFaviconFile);
+        self::assertFileEquals($faviconFile, $copiedFaviconFile);
+    }
+
+    private function routesArePopulated(string $dir)
+    {
+        $routesFile = $this->getProjectRoot() . '/routes.yml';
+        self::assertFileExists($routesFile);
+
+        $copiedRoutesFile = $dir . '/routes.yml';
+
+        self::assertFileExists($copiedRoutesFile);
+        self::assertFileEquals($routesFile, $copiedRoutesFile);
+    }
+
+    private function indexIsPopulated(string $dir)
+    {
+        $indexFile = $dir . '/index.php';
+
+        self::assertFileExists($indexFile);
+        self::assertSame(
+            <<<'PHP'
+<?php
+require __DIR__ . '/vendor/drdplus/rules-skeleton/index.php';
+
+PHP
+            ,
+            file_get_contents($indexFile),
+            "Copied index $indexFile does not match expected content"
+        );
+    }
+
+    private function webDirIsCreated(string $dir)
+    {
+        $webDir = $dir . '/web';
+
+        self::assertDirectoryExists($webDir);
+        self::assertDirectoryIsWritable($webDir);
+    }
 }
